ad11f60cf17ca8e563d15e623467f11bbbcba3bd
aarnav1729 <aarnavsingh836@gmail.com>
2025-02-05 17:41:28 +0530

    up header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import LeftLogo from '../assets/spot.svg';
 import RightLogo from '../assets/logo-right.png';
 
 const HeaderContainer = styled.div`
-  
   background-color: white;
   display: flex;
   justify-content: space-between;
@@ -19,19 +19,21 @@ const LeftSection = styled.div`
   position: relative; /* Allow child elements to be positioned absolutely */
   display: flex;
   align-items: center;
+  cursor: pointer;
 `;
 
 const LeftLogoStyled = styled.img`
   position: absolute; /* Position the logo relative to the LeftSection */
   left: 0; /* Align to the left */
   top: 0;
-  z-index:1001;
+  z-index: 1001;
   transform: translateY(-50%); /* Center vertically */
   height: 300px; /* Larger size for the logo */
 `;
 
 const RightLogoStyled = styled.img`
-  height: 50px; 
+  height: 50px;
+  cursor: pointer;
 `;
 
 const Title = styled.h3`
@@ -41,12 +43,22 @@ const Title = styled.h3`
 `;
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLeftLogoClick = () => {
+    navigate('/profile');
+  };
+
+  const handleRightLogoClick = () => {
+    window.location.href = 'https://premierenergies.com';
+  };
+
   return (
     <HeaderContainer>
-      <LeftSection>
+      <LeftSection onClick={handleLeftLogoClick}>
         <LeftLogoStyled src={LeftLogo} alt="Left Logo" />
       </LeftSection>
-      <RightLogoStyled src={RightLogo} alt="Right Logo" />
+      <RightLogoStyled src={RightLogo} alt="Right Logo" onClick={handleRightLogoClick} />
     </HeaderContainer>
   );
 };
